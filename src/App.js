db26453b8cb69a567ee80007beadb59be06367f1
priyachimurkar <priyachimurkar20@gmail.com>
2022-02-07 23:33:04 +0530

    my first roboproject

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+
+
+class App extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      // robots : robots,
+      robots : [],
+      searchField : '',
+    }
+
+    console.log('Constructor is running...')
+  }
+  
+
+  onInputChange = async(val) => {
+      await this.setState({searchField : val})
+      console.log(this.state.searchField);
+  }
+
+
+  componentDidMount = () => {
+  
+    console.log("Component Did Mount is running");
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {
+          // console.log(users);
+          this.setState({robots : users})
+        })
+  }
+
+
+  render(){
+    console.log('Render is running...');
+
+    const {robots, searchField} = this.state;
+
+    const filteredRobots = robots.filter((robot) => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
+    if (!robots.length) {   
+      return <h1>Loading....</h1>
+    }
+     
+    return (
+      <div className="tc">
+        <h1>RoboProject</h1>
+        <SearchBox onInputChange={this.onInputChange}/> 
+        
+        <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+ function App() {
+//   return (
+//     <div className="tc">
+//       <h1>RoboProject</h1>
+//       <CardList robots={robots}/>
+//     </div>
+//   );
+// }
